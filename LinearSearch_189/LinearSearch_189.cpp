83d3113c3256246b48543cff1b1daa62a16ec9be
salss33 <hallosalsa03@gmail.com>
2023-04-14 11:18:27 +0700

    continue search

--- LinearSearch_189/LinearSearch_189.cpp
+++ LinearSearch_189/LinearSearch_189.cpp
@@ -52,3 +52,12 @@ void LinearSearch()
 				break;
 			}
 		}
+
+		if (i == n)
+			cout << "n" << item << " not found in the array\n";
+		cout << "\nNumber of comparisons: " << comparison << endl;
+
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'y'));
+}
